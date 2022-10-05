1028fd5d5904c39421671a554fb15353a7e6d371
Yannis <yannis.tocreau@outlook.com>
2022-10-05 09:22:05 +0200

    Fixed URL in prod appsettings

--- src/BE/WebApi/Program.cs
+++ src/BE/WebApi/Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
 {
     app.UseWebAssemblyDebugging();
     app.UseSwagger();
