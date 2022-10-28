c97bcbd2927a269c564aff245215a09274081d2d
= <=>
2022-10-28 19:22:34 +0200

    finished calendar generation algorithm

--- src/FE/Digitime.Shared.UI/Components/Common/CalendarComponent.razor.cs
+++ src/FE/Digitime.Shared.UI/Components/Common/CalendarComponent.razor.cs
@@ -8,14 +8,85 @@ public partial class CalendarComponent : ComponentBase
 {
     [Inject] HttpClient HttpClient { get; set; }
 
-    private List<DateTime> _daysOfCurrentfMonth = new();
-
-    protected override Task OnInitializedAsync()
+    protected override async Task OnInitializedAsync()
     {
-        _daysOfCurrentfMonth = GetAllDaysOfSpecifiedMonth(DateTime.Now);
-        var t = GetPublicHolidaysForSpecifiedMonthAndCountry(DateTime.Now, "FR");
 
-        return base.OnInitializedAsync();
+        // Get days in current month
+        // Get holidays
+        // Get worked days for the month from API
+        /// Calendar rendering algorithm
+        //// Create a 2 dimensions array that will hold all the values and represent
+        //// Place all the current month days into that 2D array
+        //// Get the first day of the month and place it on a 2 dimension array
+        //// Loop from this starting day to add all days until the end of month
+        //// Once the calendar is rendered in memory, trigger the UI rendering
+        ///
+        CurrentMonthCalendarDays = await GetCalendarDaysForMonth(DateTime.Now);
+        NextMonthCalendarDays = await GetCalendarDaysForMonth(DateTime.Now.AddMonths(1));
+    }
+
+    private async Task<CalendarDay[,]> GetCalendarDaysForMonth(DateTime dateTime)
+    {
+        MonthDates = GetAllDaysOfSpecifiedMonth(dateTime);
+        PublicHolidays = await GetPublicHolidaysForSpecifiedMonthAndCountry(dateTime, "FR");
+        var calendarDays = new CalendarDay[6, 7];
+        var firstDayPositionInWeek = ((int)MonthDates.FirstOrDefault().DayOfWeek) - 1;
+        var weekIndex = 0;
+        var weekdayIndex = firstDayPositionInWeek;
+
+        // Iterate through days of the month and fill an inmemory calendar
+        foreach (var day in MonthDates)
+        {
+            if (weekdayIndex == 7)
+            {
+                weekdayIndex = 0;
+                weekIndex++;
+            }
+
+            calendarDays[weekIndex, weekdayIndex] = CreateFilledCalendarDay(day);
+            weekdayIndex++;
+        }
+        
+        return calendarDays;
+    }
+
+    CalendarDay[,] CurrentMonthCalendarDays = new CalendarDay[6,7];
+    CalendarDay[,] NextMonthCalendarDays = new CalendarDay[6,7];
+    List<DateTime> MonthDates = new List<DateTime>();
+    List<DateTime> PublicHolidays = new List<DateTime>();
+
+    class CalendarDay
+    {
+        public List<WorkedProject> WorkedProjects = new List<WorkedProject>();
+        public DayOfWeek DayOfWeek { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsPublicHoliday { get; set; }
+    }
+
+    class WorkedProject
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public float WorkedHours { get; set; }
+    }
+
+    CalendarDay CreateFilledCalendarDay(DateTime dateTime)
+    {
+        return new CalendarDay()
+        {
+            WorkedProjects = new List<WorkedProject>()
+                {
+                    new WorkedProject()
+                    {
+                        Id = Guid.NewGuid(),
+                        Title = "CIRAC",
+                        WorkedHours = 8
+                    }
+                },
+            Date = dateTime,
+            DayOfWeek = dateTime.DayOfWeek,
+            IsPublicHoliday = PublicHolidays.Contains(dateTime)
+        };
     }
 
     private int CalculateDaysBetweenDates(DateTime startTime, DateTime endTime)
@@ -41,7 +112,6 @@ public partial class CalendarComponent : ComponentBase
         var publicHolidays = new List<DateTime>();
         var firstDayOfMonth = new DateTime(dateTime.Year, dateTime.Month, 1);
         var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
-        var daysInMonth = CalculateDaysBetweenDates(firstDayOfMonth, lastDayOfMonth);
         var requestUri = $"https://date.nager.at/api/v3/PublicHolidays/{dateTime.Year}/{country}";
         var response = await HttpClient.GetAsync(requestUri);
         if (response.IsSuccessStatusCode)
@@ -63,7 +133,6 @@ public partial class CalendarComponent : ComponentBase
             var statuscode = response.StatusCode;
             throw new Exception(error);
         }
-        return null;
     }
 }
 
