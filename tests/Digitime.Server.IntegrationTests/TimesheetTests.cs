31d534735f8a5fe20d85c06e8404409f0416e984
Yannis Tocreau <yannis.tocreau@consulting-for.edenred.com>
2022-12-25 20:37:30 +0100

    Started seeding data for integration tests (WIP)

--- tests/Digitime.Server.IntegrationTests/TimesheetTests.cs
+++ tests/Digitime.Server.IntegrationTests/TimesheetTests.cs
@@ -4,19 +4,26 @@ using Digitime.Server.IntegrationTests.Infrastructure;
 using Digitime.Shared.Contracts.Timesheets;
 using FluentAssertions;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
 
 namespace Digitime.Server.IntegrationTests;
 
-public class TimesheetTests
+public class TimesheetTests : IAsyncLifetime
 {
     private readonly WebApplicationFactory<Program> _factory;
     private const string _BaseEndpointUri = "/api/timesheets/entry";
+    private static MongoClient _mongoClient;
 
     public TimesheetTests()
     {
+        // We instantiate the mongo client only once for all the tests
+        if (_mongoClient is null)
+            _mongoClient = new MongoClient(Factory.Configuration["DatabaseConfiguration:ConnectionString"]);
+        
         _factory = new WebApplicationFactory<Program>();
     }
 
@@ -26,6 +33,7 @@ public class TimesheetTests
         // Arrange
         var client = _factory.WithWebHostBuilder(builder =>
         {
+            builder.UseEnvironment("Staging");
             builder.ConfigureTestServices(services =>
             {
                 services
@@ -34,7 +42,7 @@ public class TimesheetTests
                         "TestScheme", options => { });
             });
         }).CreateClient();
-        
+
         var command = new CreateTimesheetEntryRequest
         {
             TimesheetId = "6392737298425fc69e63839a",
@@ -48,5 +56,13 @@ public class TimesheetTests
 
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.Created);
+    }
+
+    public Task InitializeAsync()
+    {
+    }
+
+    public Task DisposeAsync()
+    {
     }
 }
