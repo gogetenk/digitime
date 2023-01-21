d7e043ecfff4d006f36643b86e82eb4ebe897277
Yannis Tocreau <yannis.tocreau@consulting-for.edenred.com>
2023-01-21 21:55:20 +0100

    Added dotnet MAUI app (wip)

---
+++ src/FE/Digitime.Client.Mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,8 @@
+﻿using Foundation;
+
+namespace Digitime.Client.Mobile;
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
---
+++ src/FE/Digitime.Client.Mobile/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    purge: {
+        enabled: true,
+        content: [
+            './**/*.html',
+            './**/*.razor'
+        ],
+    },
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
