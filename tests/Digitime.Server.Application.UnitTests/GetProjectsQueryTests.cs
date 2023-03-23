b5dbbc127c516ba5a3cf448ebfe25ff02f97f5bf
Yannis Tocreau <yannis.tocreau@consulting-for.edenred.com>
2023-03-23 09:55:05 +0100

    fix: TU

--- tests/Digitime.Server.Application.UnitTests/GetProjectsQueryTests.cs
+++ tests/Digitime.Server.Application.UnitTests/GetProjectsQueryTests.cs
@@ -32,7 +32,7 @@ public class GetProjectsQueryTests
 
         // Assert
         result.Should().NotBeNull();
-        result.Projects.Should().BeEquivalentTo(projectsToReturn.Adapt<List<WorkspaceDto>>());
+        result.Projects.Should().BeEquivalentTo(projectsToReturn.Adapt<List<ProjectDto>>());
     }
 
     [Fact]
