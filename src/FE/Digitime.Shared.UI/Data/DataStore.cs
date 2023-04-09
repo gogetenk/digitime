104381a96761e6b2c8c9356044f3692aee3a4d4e
Yannis Tocreau <yannis.tocreau@consulting-for.edenred.com>
2023-04-09 18:36:01 +0200

    added const

--- src/FE/Digitime.Shared.UI/Data/DataStore.cs
+++ src/FE/Digitime.Shared.UI/Data/DataStore.cs
@@ -17,13 +17,20 @@ public class DataStore : IDataStore
 {
     public ErrorNotification ErrorNotificationComponent { get; set; }
 
-    private readonly ILogger<DataStore> _logger;
     private readonly HttpClient _httpClient;
     private readonly ILocalStorageService _localStorage;
 
-    public DataStore(ILogger<DataStore> logger, HttpClient httpClient, ILocalStorageService localStorage)
+    private const string ApiTimesheetsCalendar = "api/timesheets/calendar";
+    private const string ApiProjects = "api/projects";
+    private const string ApiTimesheetsEntry = "api/timesheets/entry";
+    private const string ApiIndicators = "api/indicators";
+    private const string ApiProjectsRegister = "api/projects/register";
+    private const string ApiNotifications = "api/notifications";
+    private const string ApiWorkspaces = "api/workspaces";
+    private const string ApiProjectsInvite = "api/projects/invite";
+
+    public DataStore(HttpClient httpClient, ILocalStorageService localStorage)
     {
-        _logger = logger;
         _httpClient = httpClient;
         _localStorage = localStorage;
     }
@@ -57,7 +64,7 @@ public class DataStore : IDataStore
 
     public async Task<CalendarDto> GetCalendar(DateTime date, string country)
     {
-        var response = await _httpClient.GetAsync($"api/timesheets/calendar?country={country}&month={date.Month}&year={date.Year}");
+        var response = await _httpClient.GetAsync($"{ApiTimesheetsCalendar}?country={country}&month={date.Month}&year={date.Year}");
         if (!response.IsSuccessStatusCode)
         {
             var error = await response.Content.ReadAsStringAsync();
@@ -71,32 +78,32 @@ public class DataStore : IDataStore
 
     public async Task<CreateProjectResponse> CreateProject(CreateProjectVm project)
     {
-        return await ExecuteHttpRequestAsync<CreateProjectResponse>(async () => await _httpClient.PostAsJsonAsync($"api/projects", project));
+        return await ExecuteHttpRequestAsync<CreateProjectResponse>(async () => await _httpClient.PostAsJsonAsync(ApiProjects, project));
     }
 
     public async Task<ProjectDto> GetProjectById(string id)
     {
-        return await ExecuteHttpRequestAsync<ProjectDto>(async () => await _httpClient.GetAsync($"api/projects/{id}"));
+        return await ExecuteHttpRequestAsync<ProjectDto>(async () => await _httpClient.GetAsync($"{ApiProjects}/{id}"));
     }
 
     public async Task<GetUserProjectsResponse> GetUserProjects()
     {
-        return await ExecuteHttpRequestAsync<GetUserProjectsResponse>(async () => await _httpClient.GetAsync($"api/projects"));
+        return await ExecuteHttpRequestAsync<GetUserProjectsResponse>(async () => await _httpClient.GetAsync(ApiProjects));
     }
 
     public async Task<CreateTimesheetEntryReponse> CreateTimesheetEntry(CreateTimesheetEntryRequest request)
     {
-        return await ExecuteHttpRequestAsync<CreateTimesheetEntryReponse>(async () => await _httpClient.PostAsJsonAsync($"api/timesheets/entry", request));
+        return await ExecuteHttpRequestAsync<CreateTimesheetEntryReponse>(async () => await _httpClient.PostAsJsonAsync($"{ApiTimesheetsEntry}/entry", request));
     }
 
     public async Task<List<DashboardIndicatorsDto>> GetDashboardIndicators()
     {
-        return await ExecuteHttpRequestAsync<List<DashboardIndicatorsDto>>(async () => await _httpClient.GetAsync($"api/indicators"));
+        return await ExecuteHttpRequestAsync<List<DashboardIndicatorsDto>>(async () => await _httpClient.GetAsync($"{ApiIndicators}"));
     }
 
     public async Task RegisterWithInvitation(string invitationToken, string id)
     {
-        await ExecuteHttpRequestAsync(async () => await _httpClient.PostAsJsonAsync($"api/projects/register", new
+        await ExecuteHttpRequestAsync(async () => await _httpClient.PostAsJsonAsync($"{ApiProjects}/register", new
         {
             invitationToken,
             id
@@ -105,16 +112,16 @@ public class DataStore : IDataStore
 
     public async Task<List<NotificationDto>> GetNotificationsAsync()
     {
-        return await ExecuteHttpRequestAsync<List<NotificationDto>>(async () => await _httpClient.GetAsync($"api/notifications"));
+        return await ExecuteHttpRequestAsync<List<NotificationDto>>(async () => await _httpClient.GetAsync(ApiNotifications));
     }
 
     public async Task<WorkspaceDto> GetWorkspaceById(string id)
     {
-        return await ExecuteHttpRequestAsync<WorkspaceDto>(async () => await _httpClient.GetAsync($"api/workspaces/{id}"));
+        return await ExecuteHttpRequestAsync<WorkspaceDto>(async () => await _httpClient.GetAsync($"{ApiWorkspaces}/{id}"));
     }
 
     public async Task InviteProjectMember(InviteMemberDto inviteMember)
     {
-        await ExecuteHttpRequestAsync(async () => await _httpClient.PostAsJsonAsync($"api/projects/invite", inviteMember));
+        await ExecuteHttpRequestAsync(async () => await _httpClient.PostAsJsonAsync($"{ApiProjects}/invite", inviteMember));
     }
 }
