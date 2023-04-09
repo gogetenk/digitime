cece97838cd937d1f4763c4edcf7fe512f6e160d
Yannis Tocreau <yannis.tocreau@consulting-for.edenred.com>
2023-04-09 12:58:00 +0200

    feat: refactored DataStore class and error handling

--- src/FE/Digitime.Shared.UI/Data/DataStore.cs
+++ src/FE/Digitime.Shared.UI/Data/DataStore.cs
@@ -6,6 +6,7 @@ using Digitime.Shared.Contracts.Projects;
 using Digitime.Shared.Contracts.Timesheets;
 using Digitime.Shared.Contracts.Workspaces;
 using Digitime.Shared.Dto;
+using Digitime.Shared.Exceptions;
 using Digitime.Shared.UI.Components;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -19,47 +20,33 @@ public class DataStore : IDataStore
     private readonly ILogger<DataStore> _logger;
     private readonly HttpClient _httpClient;
     private readonly ILocalStorageService _localStorage;
-    private readonly ErrorNotification _errorNotification;
 
-    public DataStore(ILogger<DataStore> logger, HttpClient httpClient, ILocalStorageService localStorage, ErrorNotification errorNotification)
+    public DataStore(ILogger<DataStore> logger, HttpClient httpClient, ILocalStorageService localStorage)
     {
         _logger = logger;
         _httpClient = httpClient;
         _localStorage = localStorage;
-        _errorNotification = errorNotification;
     }
 
     public async Task<T> ExecuteHttpRequestAsync<T>(Func<Task<HttpResponseMessage>> httpRequest)
     {
-        try
+        var response = await httpRequest();
+        if (!response.IsSuccessStatusCode)
         {
-            var response = await httpRequest();
-
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = await response.Content.ReadAsStringAsync();
-                var statusCode = response.StatusCode;
-
-                if ((int)statusCode >= 400 && (int)statusCode < 500)
-                {
-                    _errorNotification.ShowError(statusCode, error);
-                }
-                else if ((int)statusCode >= 500)
-                {
-                    _errorNotification.ShowTechnicalError();
-                }
-
-                return default(T);
-            }
-
-            return await response.Content.ReadFromJsonAsync<T>();
+            var error = await response.Content.ReadAsStringAsync();
+            throw new BackendException(error);
         }
-        catch (Exception exc)
-        {
-            _logger.LogError(exc.Message, exc);
-            //ErrorNotificationComponent?.ShowTechnicalError();
 
-            return default(T);
+        return await response.Content.ReadFromJsonAsync<T>();
+    }
+
+    public async Task ExecuteHttpRequestAsync(Func<Task<HttpResponseMessage>> httpRequest)
+    {
+        var response = await httpRequest();
+        if (!response.IsSuccessStatusCode)
+        {
+            var error = await response.Content.ReadAsStringAsync();
+            throw new BackendException(error);
         }
     }
 
@@ -70,209 +57,86 @@ public class DataStore : IDataStore
 
     public async Task<CalendarDto> GetCalendar(DateTime date, string country)
     {
-        try
-        {
-            var response = await _httpClient.GetAsync($"api/timesheets/calendar?country={country}&month={date.Month}&year={date.Year}");
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = await response.Content.ReadAsStringAsync();
-                var statuscode = response.StatusCode;
-                return await _localStorage.GetItemAsync<CalendarDto>($"calendar_{date.Month}_{date.Year}");
-            }
-            var responseContent = await response.Content.ReadAsStringAsync();
-            await _localStorage.SetItemAsStringAsync($"calendar_{date.Month}_{date.Year}", responseContent);
-            return JsonConvert.DeserializeObject<CalendarDto>(responseContent);
-        }
-        catch (Exception exc)
-        {
-            _logger.LogError(exc.Message, exc);
-            return null;
-        }
-    }
-
-    public async Task<CreateProjectResponse> CreateProject(CreateProjectVm project)
-    {
-        try
-        {
-            var response = await _httpClient.PostAsJsonAsync($"api/projects", project);
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = await response.Content.ReadAsStringAsync();
-                var statuscode = response.StatusCode;
-            }
-            var responseContent = await response.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<CreateProjectResponse>(responseContent);
-        }
-        catch (Exception exc)
-        {
-            _logger.LogError(exc.Message, exc);
-            return null;
-        }
-    }
-
-    public async Task<ProjectDto> GetProjectById(string id)
-    {
-        try
-        {
-            var response = await _httpClient.GetAsync($"api/projects/{id}");
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = await response.Content.ReadAsStringAsync();
-                var statuscode = response.StatusCode;
-            }
-            var responseContent = await response.Content.ReadAsStringAsync();
-            var resp = JsonConvert.DeserializeObject<ProjectDto>(responseContent);
-            return resp;
-        }
-        catch (Exception exc)
-        {
-            _logger.LogError(exc.Message, exc);
-            return null;
-        }
-    }
-
-    public async Task<GetUserProjectsResponse> GetUserProjects()
-    {
-        try
-        {
-            var response = await _httpClient.GetAsync($"api/projects");
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = await response.Content.ReadAsStringAsync();
-                var statuscode = response.StatusCode;
-            }
-            var responseContent = await response.Content.ReadAsStringAsync();
-            var resp = JsonConvert.DeserializeObject<GetUserProjectsResponse>(responseContent);
-            return resp;
-        }
-        catch (Exception exc)
-        {
-            _logger.LogError(exc.Message, exc);
-            return null;
-        }
-    }
-
-    public async Task<CreateTimesheetEntryReponse> CreateTimesheetEntry(CreateTimesheetEntryRequest request)
-    {
-        try
-        {
-            var response = await _httpClient.PostAsJsonAsync($"api/timesheets/entry", request);
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = await response.Content.ReadAsStringAsync();
-                var statuscode = response.StatusCode;
-            }
-            var responseContent = await response.Content.ReadAsStringAsync();
-            var resp = JsonConvert.DeserializeObject<CreateTimesheetEntryReponse>(responseContent);
-            return resp;
-        }
-        catch (Exception exc)
-        {
-            _logger.LogError(exc.Message, exc);
-            return null;
-        }
-    }
-
-    public async Task<List<DashboardIndicatorsDto>> GetDashboardIndicators()
-    {
-        try
-        {
-            var response = await _httpClient.GetAsync($"api/indicators");
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = await response.Content.ReadAsStringAsync();
-                var statuscode = response.StatusCode;
-            }
-            var responseContent = await response.Content.ReadAsStringAsync();
-            var resp = JsonConvert.DeserializeObject<List<DashboardIndicatorsDto>>(responseContent);
-            return resp;
-        }
-        catch (Exception exc)
-        {
-            _logger.LogError(exc.Message, exc);
-            return null;
-        }
-    }
-
-    public async Task RegisterWithInvitation(string invitationToken, string id)
-    {
-        try
-        {
-            var response = await _httpClient.PostAsJsonAsync($"api/projects/register", new
-            {
-                invitationToken = invitationToken,
-                id = id
-            });
-
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = await response.Content.ReadAsStringAsync();
-                var statuscode = response.StatusCode;
-            }
-        }
-        catch (Exception exc)
-        {
-            _logger.LogError(exc.Message, exc);
-        }
-    }
-
-    public async Task<List<NotificationDto>> GetNotificationsAsync()
-    {
-        try
-        {
-            var response = await _httpClient.GetAsync($"api/notifications");
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = await response.Content.ReadAsStringAsync();
-                var statuscode = response.StatusCode;
-            }
-            var responseContent = await response.Content.ReadAsStringAsync();
-            var resp = JsonConvert.DeserializeObject<List<NotificationDto>>(responseContent);
-            return resp;
-        }
-        catch (Exception exc)
-        {
-            _logger.LogError(exc.Message, exc);
-            return null;
-        }
-    }
-
-    public async Task<WorkspaceDto> GetWorkspaceById(string id)
-    {
-        try
-        {
-            var response = await _httpClient.GetAsync($"api/workspaces/{id}");
-            if (!response.IsSuccessStatusCode)
-            {
-                var error = await response.Content.ReadAsStringAsync();
-                var statuscode = response.StatusCode;
-            }
-            var responseContent = await response.Content.ReadAsStringAsync();
-            var resp = JsonConvert.DeserializeObject<WorkspaceDto>(responseContent);
-            return resp;
-        }
-        catch (Exception exc)
-        {
-            _logger.LogError(exc.Message, exc);
-            return null;
-        }
-    }
-
-    public async Task InviteProjectMember(InviteMemberDto inviteMember)
-    {
-        //await ExecuteHttpRequestAsync(async () =>
-        //{
-        //    return await _httpClient.PostAsJsonAsync($"api/projects/invite", inviteMember);
-        //});
-
-        var response = await _httpClient.PostAsJsonAsync($"api/projects/invite", inviteMember);
+        var response = await _httpClient.GetAsync($"api/timesheets/calendar?country={country}&month={date.Month}&year={date.Year}");
         if (!response.IsSuccessStatusCode)
         {
             var error = await response.Content.ReadAsStringAsync();
             var statuscode = response.StatusCode;
-            //ErrorNotificationComponent.ShowError(statuscode, error);
-            throw new ApplicationException(error);
+            return await _localStorage.GetItemAsync<CalendarDto>($"calendar_{date.Month}_{date.Year}");
         }
+        var responseContent = await response.Content.ReadAsStringAsync();
+        await _localStorage.SetItemAsStringAsync($"calendar_{date.Month}_{date.Year}", responseContent);
+        return JsonConvert.DeserializeObject<CalendarDto>(responseContent);
+    }
 
+    public async Task<CreateProjectResponse> CreateProject(CreateProjectVm project)
+    {
+        return await ExecuteHttpRequestAsync<CreateProjectResponse>(async () =>
+            await _httpClient.PostAsJsonAsync($"api/projects", project)
+        );
+    }
+
+    public async Task<ProjectDto> GetProjectById(string id)
+    {
+        return await ExecuteHttpRequestAsync<ProjectDto>(async () =>
+            await _httpClient.GetAsync($"api/projects/{id}")
+        );
+    }
+
+    public async Task<GetUserProjectsResponse> GetUserProjects()
+    {
+        return await ExecuteHttpRequestAsync<GetUserProjectsResponse>(async () =>
+            await _httpClient.GetAsync($"api/projects")
+        );
+    }
+
+    public async Task<CreateTimesheetEntryReponse> CreateTimesheetEntry(CreateTimesheetEntryRequest request)
+    {
+        return await ExecuteHttpRequestAsync<CreateTimesheetEntryReponse>(async () =>
+             await _httpClient.PostAsJsonAsync($"api/timesheets/entry", request)
+        );
+    }
+
+    public async Task<List<DashboardIndicatorsDto>> GetDashboardIndicators()
+    {
+        return await ExecuteHttpRequestAsync<List<DashboardIndicatorsDto>>(async () =>
+            await _httpClient.GetAsync($"api/indicators")
+        );
+    }
+
+    public async Task RegisterWithInvitation(string invitationToken, string id)
+    {
+        await ExecuteHttpRequestAsync(async () =>
+        {
+            return await _httpClient.PostAsJsonAsync($"api/projects/register", new
+            {
+                invitationToken,
+                id
+            });
+        });
+    }
+
+    public async Task<List<NotificationDto>> GetNotificationsAsync()
+    {
+        return await ExecuteHttpRequestAsync<List<NotificationDto>>(async () =>
+        {
+            return await _httpClient.GetAsync($"api/notifications");
+        });
+    }
+
+    public async Task<WorkspaceDto> GetWorkspaceById(string id)
+    {
+        return await ExecuteHttpRequestAsync<WorkspaceDto>(async () =>
+        {
+            return await _httpClient.GetAsync($"api/workspaces/{id}");
+        });
+    }
+
+    public async Task InviteProjectMember(InviteMemberDto inviteMember)
+    {
+        await ExecuteHttpRequestAsync(async () =>
+        {
+            return await _httpClient.PostAsJsonAsync($"api/projects/invite", inviteMember);
+        });
     }
 }
---
+++ src/Shared/Exceptions/BackendException.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+namespace Digitime.Shared.Exceptions;
+public class BackendException : Exception
+{
+    public BackendException()
+    {
+    }
+
+    public BackendException(string? message) : base(message)
+    {
+    }
+
+    public BackendException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected BackendException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
